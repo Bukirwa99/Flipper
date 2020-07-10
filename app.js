f75bc50ea31d852f3ee438424d595af4aaa3a5a5
bukirwa99 <angellabukirwa99@gmail.com>
2020-07-10 18:27:29 +0300

    building

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn =document.getElementbyId('btn');
+const color =document.querySelector('.colors');
+btn.addeventListener('click',function(){
+
+});
