0782b26a0871cbb05bd09d61b318267f05b22368
bukirwa99 <angellabukirwa99@gmail.com>
2020-07-11 17:50:44 +0300

    functionings

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-const btn =document.getElementbyId('btn');
-const color =document.querySelector('.colors');
-btn.addeventListener('click',function(){
+const btn =document.getElementById('btn');
+const color =document.querySelector('.color');
+btn.addEventListener('click', function(){
+    const randomNumber=getRandomNumber();
+    console.log(randomNumber)
+    document.body.style.backgroundColor=colors[randomNumber];
+    color.textContent=colors[randomNumber];
 
 });
+function getRandomNumber(){
+    return Math.floor(Math.random()* colors.lenght); 
+}
