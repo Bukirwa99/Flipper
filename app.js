c8e227f38f2c99e9d3c4a736e8b5e0fde3ac648b
bukirwa99 <angellabukirwa99@gmail.com>
2020-07-11 21:56:43 +0300

    flipping

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-const btn =document.getElementById('btn');
-const color =document.querySelector('.color');
+const btn =document.getElementById("btn");
+const color =document.querySelector(".color");
 btn.addEventListener('click', function(){
-    const randomNumber=getRandomNumber();
+    const randomNumber= getRandomNumber();
     console.log(randomNumber)
     document.body.style.backgroundColor=colors[randomNumber];
     color.textContent=colors[randomNumber];
 
 });
 function getRandomNumber(){
-    return Math.floor(Math.random()* colors.lenght); 
+    return Math.floor(Math.random()* colors.length); 
 }
